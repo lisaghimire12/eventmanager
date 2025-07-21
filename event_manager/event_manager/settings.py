0195a0d559c9c17b6d10e8dcc6e59923d399285b
lisaghimire12 <lisa.ghimire1@gmail.com>
2025-07-21 14:23:16 +0530

    Added static file

--- event_manager/event_manager/settings.py
+++ event_manager/event_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c2bf&hdbt5h*!g*cny8e^l*mh2fyd(3$#=enrigddy5c6u(k^#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lisaghimire12.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'lisaghimire12.pythonanywhere.com']
 
 
 # Application definition
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
